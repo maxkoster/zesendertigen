ed51dcd2fad6daa2b0b9f00827a559acd7a044d6
maxkoster <maxkoster@hotmail.com>
2021-02-06 17:04:31 +0100

    Working version with odds calculation

---
+++ zesendertigen.c
@@ -0,0 +1,92 @@
+#include "zesendertigen.h"
+
+void throwDice(int no_dice, int *thrown_dice){
+    for (int i=0; i < no_dice; i++){
+        thrown_dice[i] = rand() % 6 + 1;
+    }
+}
+
+void odds36(double tries){
+    double odds = 0;
+    int total;
+    int no_dice;
+
+    int sixes;
+    for (int try = 0; try < tries; try++){
+        bool running = true;
+        no_dice = 6;
+        total = 0;
+        while (running==true){
+            sixes = 0;
+            int thrown_dice[no_dice];
+            throwDice(no_dice, thrown_dice);
+            int loop_dice = no_dice;
+            for (int i=0; i < loop_dice; i++){
+                if (thrown_dice[i] == 6){
+                    sixes++;
+                    no_dice--;
+                    total += 6;    
+                }
+            }
+            if (sixes == 0){
+                running = false;
+            }
+        
+        }
+    if (total == 36){
+        odds++;
+    }
+    }
+    printf("The odds are:%lf\n",odds/tries);
+}
+
+void averageThrow(int min_number, double tries){
+    int no_dice;
+    int total;
+    double average_throw = 0;
+
+    for (int try = 0; try < tries; try++){
+        bool running = true;
+        no_dice = 6;
+        total = 0;
+        while (running==true){
+            int thrown_dice[no_dice];
+            throwDice(no_dice, thrown_dice);
+            int loop_dice = no_dice;
+            bool die_taken = false;
+            int max_die = 0;
+            for (int i=0; i < loop_dice; i++){
+                if (thrown_dice[i] >= min_number){
+                    no_dice--;
+                    total += thrown_dice[i];
+                    die_taken = true;
+                }
+                if (thrown_dice[i] > max_die){
+                    max_die = thrown_dice[i];
+                }
+            }
+            if (die_taken==false){
+                total += max_die;
+                no_dice--;
+            }
+            if (no_dice == 0){
+                running = false;
+            }
+        }
+        average_throw += total;
+    }
+    printf("When taking out the minimum number of %i\n",min_number);
+    printf("The average total is: %lf\n", average_throw/tries);
+}
+
+int main(){
+    srand(time(0));
+    // odds36(1000000);
+    averageThrow(6,10000000);
+    averageThrow(5,10000000);
+    averageThrow(4,10000000);
+    averageThrow(3,10000000);
+    averageThrow(2,10000000);
+    averageThrow(1,10000000);
+    
+}
---
+++ zesendertigen.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+void throwDice(int no_dice, int *thrown_dice);
+void odds36(double tries);
+void averageThrow(int min_number, double tries);
+int main();
